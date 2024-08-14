7ef24496b6de82827a4fc2dfcfb1d619e2603e15
Felipe Flores <felipeguilhermefs@gmail.com>
2024-08-14 21:51:17 +0200

    Refactor Queuento lua object/class style

--- src/ds/queue.lua
+++ src/ds/queue.lua
@@ -1,3 +1,16 @@
+-- Queue:
+--	.new() - Creates a new instance of a queue
+--	:enqueue(item) - Adds an item to the back of the queue
+--		item: any item
+--	:dequeue() - Removes and returns the item in the front of the queue
+--		Returns nil if empty.
+--	:peek() - Returns the item in the front of the queue
+--		Returns nil if empty.
+--	:empty() - Returns if the heap is empty or not
+--
+local Queue = { __front = nil }
+Queue.__index = Queue
+
 local function Node(value, next)
 	return {
 		value = value,
@@ -5,49 +18,50 @@ local function Node(value, next)
 	}
 end
 
-local function Queue()
-	local q = {}
+function Queue:enqueue(item)
+	local node = Node(item, nil)
 
-	q.empty = function()
-		return not q.front
+	if self.__back then
+		self.__back.next = node
+	else
+		self.__front = node
+	end
+	self.__back = node
+end
+
+function Queue:dequeue()
+	if self:empty() then
+		return nil
 	end
 
-	q.enqueue = function(value)
-		local newNode = Node(value, nil)
-		if q.back then
-			q.back.next = newNode
-		else
-			q.front = newNode
-		end
-		q.back = newNode
+	local value = self.__front.value
+
+	if self.__front == self.__back then
+		self.__front = nil
+		self.__back = nil
+	else
+		self.__front = self.__front.next
 	end
 
-	q.dequeue = function()
-		if not q.front then
-			return nil
-		end
+	return value
+end
 
-		local value = q.front.value
-
-		if q.front == q.back then
-			q.front = nil
-			q.back = nil
-		else
-			q.front = q.front.next
-		end
-
-		return value
+function Queue:peek()
+	if self:empty() then
+		return nil
+	else
+		return self.__front.value
 	end
+end
 
-	q.peek = function()
-		if not q.front then
-			return nil
-		else
-			return q.front.value
-		end
-	end
+function Queue:empty()
+	return not self.__front
+end
 
-	return q
+function Queue.new()
+	local new = {}
+	setmetatable(new, Queue)
+	return new
 end
 
 return Queue
