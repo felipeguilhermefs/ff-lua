48a559006ed50a5e399f855edbdf1d6f8aa7b388
Felipe Flores <felipeguilhermefs@gmail.com>
2024-08-16 00:40:17 +0200

    Fix memoize

--- src/func/memoize_test.lua
+++ src/func/memoize_test.lua
@@ -1,64 +1,70 @@
-lu = require "luaunit"
-spy = require "spy"
-memoize = require "memoize"
+local lu = require("luaunit")
+local spy = require("ff.spy")
+local memoize = require("memoize")
 
+function Test_RepeatedCallsSingleArg_ShouldCallOnce()
+	local fn = spy(function()
+		return 1
+	end)
 
-function test_repeatedCallsSingleArg_shouldCallOnce()
-  local fn = spy(function (a) return 1 end)
+	lu.assertEquals(fn.calls, 0)
 
-  lu.assertEquals(fn.calls, 0)
+	local mfn = memoize(fn)
+	mfn("a")
+	mfn("a")
+	mfn("a")
+	mfn("a")
 
-  local mfn = memoize(fn)
-  mfn("a")
-  mfn("a")
-  mfn("a")
-  mfn("a")
-
-  lu.assertEquals(fn.calls, 1)
+	lu.assertEquals(fn.calls, 1)
 end
 
-function test_repeatedCallsMultipleArg_shouldCallOnce()
-  local fn = spy(function (a, b, c) return 1 end)
+function Test_RepeatedCallsMultipleArg_ShouldCallOnce()
+	local fn = spy(function()
+		return 1
+	end)
 
-  lu.assertEquals(fn.calls, 0)
+	lu.assertEquals(fn.calls, 0)
 
-  local mfn = memoize(fn)
-  mfn("a", "b", "c")
-  mfn("a", "b", "c")
-  mfn("a", "b", "c")
-  mfn("a", "b", "c")
+	local mfn = memoize(fn)
+	mfn("a", "b", "c")
+	mfn("a", "b", "c")
+	mfn("a", "b", "c")
+	mfn("a", "b", "c")
 
-  lu.assertEquals(fn.calls, 1)
+	lu.assertEquals(fn.calls, 1)
 end
 
-function test_differentCalls_shouldCallAll()
-  local fn = spy(function (a, b, c) return 1 end)
+function Test_DifferentCalls_ShouldCallAll()
+	local fn = spy(function()
+		return 1
+	end)
 
-  lu.assertEquals(fn.calls, 0)
+	lu.assertEquals(fn.calls, 0)
 
-  local mfn = memoize(fn)
-  mfn("a", "b", "c")
-  mfn("b", "c", "a")
-  mfn("c", "a", "b")
+	local mfn = memoize(fn)
+	mfn("a", "b", "c")
+	mfn("b", "c", "a")
+	mfn("c", "a", "b")
 
-  lu.assertEquals(fn.calls, 3)
+	lu.assertEquals(fn.calls, 3)
 end
 
-function test_nonFunction_returnNil()
-  lu.assertNil(memoize(nil))
-  lu.assertNil(memoize(true))
-  lu.assertNil(memoize("abc"))
-  lu.assertNil(memoize(9))
-  lu.assertNil(memoize({1, 2, 3}))
+function Test_NonFunction_ReturnNil()
+	lu.assertNil(memoize(nil))
+	lu.assertNil(memoize(true))
+	lu.assertNil(memoize("abc"))
+	lu.assertNil(memoize(9))
+	lu.assertNil(memoize({ 1, 2, 3 }))
 end
 
-function test_cacheNonTable_returnNil()
-  local fn = function () return 1 end
+function Test_CacheNonTable_ReturnNil()
+	local fn = function()
+		return 1
+	end
 
-  lu.assertNil(memoize(fn, true))
-  lu.assertNil(memoize(fn, "abc"))
-  lu.assertNil(memoize(fn, 9))
+	lu.assertNil(memoize(fn, true))
+	lu.assertNil(memoize(fn, "abc"))
+	lu.assertNil(memoize(fn, 9))
 end
 
 os.exit(lu.LuaUnit.run())
-
