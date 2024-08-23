f7b70f29f4759441fe75e6d0813f5690335b4727
Felipe Flores <felipeguilhermefs@gmail.com>
2024-08-23 16:18:22 +0200

    Add docs to LRU cache

--- src/cache/lru.lua
+++ src/cache/lru.lua
@@ -1,84 +1,165 @@
--- LRUCache:
---	.new(capacity) - Creates a new instance of a cache
---		capacity: number of items that the cache can hold
---	:get(key) - Returns a item from the cache associated to the key. Returns nil if not found.
---		key: any hashable key
---	:put(key, value) - Adds a key-value pair to the cache.
---		key: any hashable key
---		value: any value
---	:evict(key) - Removes a item from the cache associated to the key.
---		key: any hashable key.
---
-
 local HashMap = require("ff.collections.hashmap")
 
-local function Entry(key, value, prev, next)
-	return { key = key, value = value, prev = prev, next = next }
+---@class (private) Entry
+---@field key   any    Stores the key that is used for the lookup.
+---@field value any    Stores the value of this entry.
+---@field prev  Entry? Points to a more recently used entry.
+---@field next  Entry? Points to a less recently used entry.
+local Entry = {}
+Entry.__index = Entry
+
+-----------------------------------------------------------------------------
+---Creates a new instance of Entry
+---
+---@param  key   any  Key that will be used for a lookup.
+---@param  value any  Value to be stored.
+---
+---@return Entry      New instance
+-----------------------------------------------------------------------------
+function Entry.new(key, value)
+	return setmetatable({ key = key, value = value }, Entry)
 end
 
-local LRUCache = { __cap = nil, __head = nil, __tail = nil, __items = nil }
+---@class LRUCache
+---@field private _cap   number               Cache's maximum size.
+---@field private _items HashMap<any, Entry>  Map for quick lookup.
+---@field private _head  Entry                Entry at the front of the queue.
+---                                           It points to the most recent entry.
+---@field private _tail  Entry                Entry at the back of the queue.
+---                                           It points to the least recent entry.
+local LRUCache = {}
 LRUCache.__index = LRUCache
 
+-----------------------------------------------------------------------------
+---Creates a new instance of the LRU cache
+---
+---@param  capacity number Maximum size desired for this cache. Should be a positive inumber.
+---
+---@return LRUCache        New instance
+-----------------------------------------------------------------------------
+function LRUCache.new(capacity)
+	assert(type(capacity) == "number", "Capacity should be a number")
+	assert(capacity > 0, "Capacity should be positive")
+
+	-- head and tail are fixed to make adding and removing from the
+	-- queue simpler, as checking the borders for `nil` is not needed
+	-- since the values are always there.
+	local head = Entry.new(0, 0)
+	local tail = Entry.new(0, 0)
+	head.next = tail
+	tail.prev = head
+
+	return setmetatable({
+		_cap = capacity,
+		_items = HashMap.new(),
+		_head = head,
+		_tail = tail,
+	}, LRUCache)
+end
+
+-----------------------------------------------------------------------------
+---Returns the value from the cache that is associated with the key.
+---
+---@param  key? any  Key used for lookup the value in the cache
+---
+---@return any       Value associated with key, or `nil` if not found
+-----------------------------------------------------------------------------
 function LRUCache:get(key)
-	local entry = self.__items:get(key)
+	if key == nil then
+		return nil
+	end
+
+	local entry = self._items:get(key)
 	if entry then
-		self:__remove(entry)
-		self:__add(entry)
+		self:_remove(entry)
+		self:_add(entry)
 		return entry.value
 	end
 end
 
+-----------------------------------------------------------------------------
+---Adds a value to the cache associated by a lookup key.
+---
+---@param  key?    any Key that will be used for lookup the value in the cache, `nil` will be ignored.
+---@param  value?  any Value to be stored in the cache, `nil` will be ignored.
+---
+---@return boolean     `true` if value was added, false otherwise.
+-----------------------------------------------------------------------------
 function LRUCache:put(key, value)
-	local entry = self.__items:get(key)
+	if key == nil or value == nil then
+		return false
+	end
+
+	local entry = self._items:get(key)
 	if entry then
-		self:__remove(entry)
+		self:_remove(entry)
 		entry.value = value
-		self:__add(entry)
+		self:_add(entry)
 	else
-		if self.__items:len() >= self.__cap then
-			self:__remove(self.__tail.prev)
+		-- just need to check the capacity if a new entry is added.
+		if self._items:len() >= self._cap then
+			self:_remove(self._tail.prev)
 		end
 
-		self:__add(Entry(key, value))
+		self:_add(Entry.new(key, value))
 	end
+	return true
 end
 
+-----------------------------------------------------------------------------
+---Removes a value from the cache that is associated with the key.
+---
+---@param  key? any Key used for lookup the value in the cache
+---
+---@return boolean  `true` if value was present, false otherwise.
+-----------------------------------------------------------------------------
 function LRUCache:evict(key)
-	local entry = self.__items:get(key)
-	if entry then
-		self:__remove(entry)
+	if key == nil then
+		return false
 	end
+
+	local entry = self._items:get(key)
+	if entry == nil then
+		return false
+	end
+
+	self:_remove(entry)
+	return true
 end
 
-function LRUCache:__add(entry)
-	local temp = self.__head.next
+-----------------------------------------------------------------------------
+---Appends an entry to the head and attach it to the map for quick lookup.
+---
+---@param  entry Entry  Most recently used entry that needs to go to the
+---                        front of the queue.
+---
+---@private
+-----------------------------------------------------------------------------
+function LRUCache:_add(entry)
+	-- since we have the head fixed, we just need to make it point to
+	-- the most recently used entry.
+	local temp = self._head.next
 	entry.next = temp
 	temp.prev = entry
-	self.__head.next = entry
-	entry.prev = self.__head
-	self.__items:put(entry.key, entry)
+	self._head.next = entry
+	entry.prev = self._head
+	self._items:put(entry.key, entry)
 end
 
-function LRUCache:__remove(entry)
+-----------------------------------------------------------------------------
+---Drops an entry from the queue and from the map.
+---
+---@param  entry Entry  Most recently used entry that needs to go to the
+---                        front of the queue.
+---
+---@private
+-----------------------------------------------------------------------------
+function LRUCache:_remove(entry)
+	-- since we have head and tail fixed we can ignore common corner cases
+	-- of removing from a doubly linked list.
 	entry.prev.next = entry.next
 	entry.next.prev = entry.prev
-	self.__items:remove(entry.key)
-end
-
-function LRUCache.new(capacity)
-	local new = {}
-	setmetatable(new, LRUCache)
-
-	local head = Entry(0, 0)
-	local tail = Entry(0, 0)
-	head.next = tail
-	tail.prev = head
-
-	new.__head = head
-	new.__tail = tail
-	new.__cap = capacity
-	new.__items = HashMap.new()
-	return new
+	self._items:remove(entry.key)
 end
 
 return LRUCache
--- src/cache/lru_test.lua
+++ src/cache/lru_test.lua
@@ -6,23 +6,23 @@ function TestUnderCapacity()
 
 	lu.assertNil(cache:get("a"))
 
-	cache:put("a", 1)
+	lu.assertTrue(cache:put("a", 1))
 	lu.assertEquals(1, cache:get("a"))
-	cache:put("b", 2)
-	cache:put("c", 3)
+	lu.assertTrue(cache:put("b", 2))
+	lu.assertTrue(cache:put("c", 3))
 	lu.assertEquals(1, cache:get("a"))
 end
 
 function TestRotationWhenOverCapacity()
 	local cache = LRUCache.new(2)
 
-	cache:put("a", 1)
+	lu.assertTrue(cache:put("a", 1))
 	lu.assertEquals(1, cache:get("a"))
 
-	cache:put("b", 2)
+	lu.assertTrue(cache:put("b", 2))
 	lu.assertEquals(2, cache:get("b"))
 
-	cache:put("c", 3)
+	lu.assertTrue(cache:put("c", 3))
 	lu.assertEquals(3, cache:get("c"))
 
 	lu.assertNil(cache:get("a"))
@@ -32,10 +32,10 @@ end
 function TestDropLeastRecentUsed()
 	local cache = LRUCache.new(2)
 
-	cache:put("a", 1)
-	cache:put("b", 2)
-	cache:get("a")
-	cache:put("c", 3)
+	lu.assertTrue(cache:put("a", 1))
+	lu.assertTrue(cache:put("b", 2))
+	lu.assertEquals(1, cache:get("a"))
+	lu.assertTrue(cache:put("c", 3))
 
 	lu.assertEquals(1, cache:get("a"))
 	lu.assertNil(cache:get("b"))
@@ -44,9 +44,9 @@ end
 function TestUpdateValueWithoutDroping()
 	local cache = LRUCache.new(2)
 
-	cache:put("a", 1)
-	cache:put("b", 2)
-	cache:put("a", 3)
+	lu.assertTrue(cache:put("a", 1))
+	lu.assertTrue(cache:put("b", 2))
+	lu.assertTrue(cache:put("a", 3))
 
 	lu.assertEquals(3, cache:get("a"))
 	lu.assertEquals(2, cache:get("b"))
@@ -55,15 +55,36 @@ end
 function TestEvict()
 	local cache = LRUCache.new(2)
 
-	cache:put("a", 1)
-	cache:put("b", 2)
-	cache:evict("b")
-	cache:evict("a")
-	cache:put("c", 3)
+	lu.assertTrue(cache:put("a", 1))
+	lu.assertTrue(cache:put("b", 2))
+	lu.assertTrue(cache:evict("b"))
+	lu.assertTrue(cache:evict("a"))
+	lu.assertTrue(cache:put("c", 3))
 
 	lu.assertEquals(3, cache:get("c"))
 	lu.assertNil(cache:get("a"))
 	lu.assertNil(cache:get("b"))
+end
+
+function TestNil()
+	local cache = LRUCache.new(2)
+
+	lu.assertFalse(cache:put(nil, "a"))
+	lu.assertFalse(cache:put("a", nil))
+
+	lu.assertNil(cache:get(nil))
+	lu.assertNil(cache:get("a"))
+
+	lu.assertFalse(cache:evict("a"))
+	lu.assertFalse(cache:evict(nil))
+end
+
+function TestCapacity()
+	lu.assertError(LRUCache.new, "a")
+	lu.assertError(LRUCache.new, nil)
+	lu.assertError(LRUCache.new, true)
+	lu.assertError(LRUCache.new, -1)
+	lu.assertError(LRUCache.new, 0)
 end
 
 os.exit(lu.LuaUnit.run())
